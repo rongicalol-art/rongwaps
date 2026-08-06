6335632964ed577cda415ddaa3b0a544f76899f4
Ron <rongicalol@gmail.com>
2026-08-07 01:11:16 +0800

    fix

--- src/lib/widgets/LayoutShell.tsx
+++ src/lib/widgets/LayoutShell.tsx
@@ -1,3 +1,4 @@
+import React, { useEffect } from 'react';
 import { AnimatePresence, motion } from 'motion/react';
 import { SideNav, type SideNavProps } from './SideNav';
 import { DynamicBackground } from './DynamicBackground';
